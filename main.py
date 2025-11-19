121690a3a993090bf3fc690f079ae873f3fdc931
Michael Betker <betkerjmichael@gmail.com>
2025-11-18 22:49:34 -0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from logisticRegression.dataPrep import dataPrep
+from logisticRegression.logisticRegression import logisticRegression
+
+X_train, X_test, y_train, y_test = dataPrep()
+
+model = logisticRegression(learningRate=0.1, numIterations=2000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+acc = (y_pred == y_test).mean()
+print("Test accuracy:", acc)
+
+print("Train set size:", len(y_train))
+print("Test set size:", len(y_test))
+print(confusion_matrix(y_test, y_pred))
+print("Positive (1) ratio in train:", np.mean(y_train))
+print("Positive (1) ratio in test:", np.mean(y_test))
+
+print(classification_report(y_test, y_pred))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+'''
+
+# Example: single-sample prediction from raw inputs
+example = {
+    "Age": 28,
+    "Gender": "Male",
+    "Area": "Lalbagh",
+    "AreaType": "Undeveloped",
+    "HouseType": "Tinshed",
+    "District": "Dhaka",
+    "NS1": 0,                 # antibody results as in dataset encoding (0/1 or numeric)
+    "IgG": 0,
+    "IgM": 0,
+}
+row = makeFeatureRow(metadata, example)
+proba = model.predictProb(row)[0]
+pred = int(proba >= 0.5)
+print("Single-sample prob of Outcome=1:", proba)
+print("Single-sample class (threshold 0.5):", pred)
+
+'''
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
